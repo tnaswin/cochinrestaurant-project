a2c05b6d684dbb74c877e3e8bf46ff55fc33d2e5
tnaswin <tnaswin007@gmail.com>
2020-05-27 18:09:26 +0530

    Add Forms and widgets

--- cochinrestaurant/pizza/forms.py
+++ cochinrestaurant/pizza/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Pizza
+from .models import Pizza, Size
 
 # class PizzaForm(forms.Form):
 #     topping1 = forms.CharField(label='Topping 1', max_length=100)
@@ -7,7 +7,12 @@ from .models import Pizza
 #     size = forms.ChoiceField(label='Size', choices=[('Small','Small'),('Medium','Medium'),('Large','Large')])
 
 class PizzaForm(forms.ModelForm):
+    
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
         labels = {'topping1':'Topping 1','topping2':'Topping 2'}
+
+
+class MultiplePizzaForm(forms.Form):
+    number = forms.IntegerField(min_value=2, max_value=6)
